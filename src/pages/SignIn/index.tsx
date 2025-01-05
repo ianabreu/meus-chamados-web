de3841a5c17016472ec90238839012a500ac79c1
Ian Abreu <ianabreudev@gmail.com>
2025-01-05 05:54:54 -0300

    add SignIn and SignUp pages

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import LOGO from "../../assets/logo.png";
+import "./styles-signin.css";
+
 export default function SignIn() {
-  return <div>Página Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={LOGO} alt="Logo do Sistema de Chamados" />
+        </div>
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="email"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="******"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </form>
+        <Link to="/register">Criar uma conta</Link>
+      </div>
+    </div>
+  );
 }
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,3 +1,42 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import LOGO from "../../assets/logo.png";
+import "../SignIn/styles-signin.css";
+
 export default function SignUp() {
-  return <div>Página Cadastro</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={LOGO} alt="Logo do Sistema de Chamados" />
+        </div>
+        <form>
+          <h1>Nova Conta</h1>
+          <input
+            type="text"
+            placeholder="Seu nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="******"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Cadastrar</button>
+        </form>
+        <Link to="/">Já possui uma conta?</Link>
+      </div>
+    </div>
+  );
 }
