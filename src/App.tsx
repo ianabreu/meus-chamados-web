c7154e3a468af116341ecb2e2714fe54567c5233
Ian Abreu <ianabreudev@gmail.com>
2025-01-06 04:54:06 -0300

    add lib react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import { RoutesApp } from "./routes";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <BrowserRouter>
       <RoutesApp />
+      <Toaster position="top-center" />
     </BrowserRouter>
   );
 }
