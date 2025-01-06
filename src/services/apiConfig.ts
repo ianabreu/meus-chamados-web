bb65900ee34800484d17c86d456a122c4d547ab6
Ian Abreu <ianabreudev@gmail.com>
2025-01-06 04:55:13 -0300

    config error interceptors in axios

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -1,7 +1,23 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3333",
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response?.status === 400 && error.response.data) {
+      const { message } = error.response.data as { message: string };
+      return Promise.reject(new Error(message));
+    }
+    if (error.response?.status === 401 && error.response.data) {
+      return Promise.reject(new Error("Unauthorized"));
+    }
+    return Promise.reject(new Error("Internal server error"));
+  }
+);
 
 export { api };
