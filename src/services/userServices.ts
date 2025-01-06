faa94f2a8c79601ea72ccc0c98aadf787541fb9f
Ian Abreu <ianabreudev@gmail.com>
2025-01-06 04:56:51 -0300

    add persistency in auth store

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,14 +1,23 @@
+/* eslint-disable no-useless-catch */
 import { AuthUserDTO, CreateUserDTO, User } from "../@types/User";
 import { api } from "./apiConfig";
 
 const userServices = {
   async create({ email, name, password }: CreateUserDTO): Promise<User> {
-    const response = await api.post("/users", { email, name, password });
-    return response.data;
+    try {
+      const response = await api.post("/users", { email, name, password });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   },
   async login({ email, password }: AuthUserDTO): Promise<User> {
-    const response = await api.post("/session", { email, password });
-    return response.data;
+    try {
+      const response = await api.post("/session", { email, password });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   },
 };
 
--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,6 +1,8 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import { AuthUserDTO, CreateUserDTO, User } from "../@types/User";
 import { userServices } from "../services/userServices";
+import toast from "react-hot-toast";
 
 type AuthStore = {
   user: User | null;
@@ -10,22 +12,41 @@ type AuthStore = {
   signIn: (credentials: AuthUserDTO) => Promise<void>;
   signOut: () => Promise<void>;
 };
-export const useAuthStore = create<AuthStore>((set) => ({
-  user: null,
-  signed: false,
-  loadingAuth: false,
-  signUp: async (credentials) => {
-    set({ loadingAuth: true });
-    await userServices.create(credentials);
-    set({ loadingAuth: false });
-  },
-  signIn: async (credentials) => {
-    set({ loadingAuth: true });
-    const userData = await userServices.login(credentials);
-    set({ user: userData, signed: !!userData });
-    set({ loadingAuth: false });
-  },
-  signOut: async () => {
-    set({ user: null, signed: false });
-  },
-}));
+
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      signed: false,
+      loadingAuth: false,
+      signUp: async (credentials) => {
+        set({ loadingAuth: true });
+        try {
+          await userServices.create(credentials);
+          toast.success("Usuário cadastrado com sucesso!");
+          set({ loadingAuth: false });
+        } catch (error) {
+          set({ loadingAuth: false });
+          throw error;
+        }
+      },
+      signIn: async (credentials) => {
+        set({ loadingAuth: true });
+        try {
+          const userData = await userServices.login(credentials);
+          set({ user: userData, signed: !!userData, loadingAuth: false });
+        } catch (error) {
+          set({ loadingAuth: false });
+          throw error;
+        }
+      },
+      signOut: async () => {
+        set({ user: null, signed: false });
+      },
+    }),
+    {
+      name: "@my_callings",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
