269b85f32fc9759a44617db79a1ad7d606862043
Ian Abreu <ianabreudev@gmail.com>
2025-01-16 05:54:57 -0300

    create Customers creation page

--- src/pages/Customers/index.tsx
+++ src/pages/Customers/index.tsx
@@ -2,28 +2,89 @@ import { FiUser } from "react-icons/fi";
 import Title from "../../components/Title";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
-import { useRef } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cnpjMask } from "../../utils/masks/cnpj";
+import { createCustomerDTO } from "../../@types/Customer";
+import { createCustomerSchema } from "../../schemas/Customer/createCustomerSchema";
+import { customerServices } from "../../services/customerServices";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function Customers() {
-  const nameRef = useRef<HTMLInputElement | null>(null);
-  const CnpjRef = useRef<HTMLInputElement | null>(null);
-  const AdressRef = useRef<HTMLInputElement | null>(null);
-  function handleRegister(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    alert("chamou");
-  }
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setError,
+    formState: { errors },
+  } = useForm<createCustomerDTO>({
+    resolver: zodResolver(createCustomerSchema),
+    mode: "onSubmit",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit: SubmitHandler<createCustomerDTO> = (data) => {
+    setLoading(true);
+    customerServices
+      .create(data)
+      .then(() => {
+        toast.success("Cliente cadastrado com sucesso.");
+        reset();
+      })
+      .catch((e) => {
+        if (e instanceof Error) {
+          if (e.message === "CNPJ já cadastrado.") {
+            setError("cnpj", { message: e.message });
+          } else {
+            toast.error(e.message);
+          }
+        } else {
+          toast.error("Erro ao cadastrar cliente");
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <>
       <Title icon={<FiUser size={25} />}>Clientes</Title>
-      <form className="profile-area" onSubmit={handleRegister}>
+      <form className="profile-area" onSubmit={handleSubmit(onSubmit)}>
         <div className="input-area">
-          <label htmlFor="">Nome</label>
-          <Input placeholder="Digite o nome do cliente" ref={nameRef} />
-          <label htmlFor="">CNPJ</label>
-          <Input placeholder="Digite o CNPJ" ref={CnpjRef} />
-          <label htmlFor="">Endereço</label>
-          <Input placeholder="Digite o endereço" ref={AdressRef} />
-          <Button type="submit">Cadastrar</Button>
+          <Input
+            label="Razão Social"
+            placeholder="Digite o nome do cliente"
+            id="companyName"
+            error={errors.companyName?.message}
+            {...register("companyName", { required: true })}
+          />
+          <Input
+            label="Nome Fantasia"
+            placeholder="Digite o nome do cliente"
+            id="tradeName"
+            error={errors.tradeName?.message}
+            {...register("tradeName", { required: true })}
+          />
+          <Input
+            label="CNPJ"
+            placeholder="Digite o CNPJ"
+            id="cnpj"
+            mask={cnpjMask}
+            error={errors.cnpj?.message}
+            {...register("cnpj", { required: true })}
+          />
+          <Input
+            label="Endereço"
+            placeholder="Digite o endereço"
+            id="address"
+            error={errors.address?.message}
+            {...register("address", { required: true })}
+          />
+          <Button type="submit" loading={loading}>
+            Cadastrar
+          </Button>
         </div>
       </form>
     </>
