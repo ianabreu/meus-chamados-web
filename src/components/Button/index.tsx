842d4684952ea79de36a007dd1fefedc8369dafc
Ian Abreu <ianabreudev@gmail.com>
2025-01-17 04:32:49 -0300

    add variant button styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,14 +3,18 @@ import "./styles-button.css";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   loading?: boolean;
+  variant?: "outline" | "filled";
 }
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, loading = false, children, ...rest }, ref) => {
+  (
+    { className, loading = false, variant = "filled", children, ...rest },
+    ref
+  ) => {
     return (
       <button
         disabled={loading}
-        className={`defaultButton ${className}`}
+        className={`defaultButton ${variant} ${className}`}
         {...rest}
         ref={ref}
       >
