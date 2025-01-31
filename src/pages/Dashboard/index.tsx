7df4b2ba87f0deb353928cfc58855b49bce9d30a
Ian Abreu <ianabreudev@gmail.com>
2025-01-30 22:50:59 -0300

    add Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,118 @@
-import { Button } from "../../components/Button";
-import { useAuthStore } from "../../store/auth";
+import Title from "../../components/Title";
+import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi";
+import Section from "../../components/Section";
+import "./styles-dashboard.css";
+import Modal from "../../components/Modal";
+import { useState } from "react";
+import NewCalling from "../../components/NewCalling";
+import { useTicketStore } from "../../store/tickets";
+import Pagination from "../../components/Pagination";
 
 export default function Dashboard() {
-  const { signOut } = useAuthStore();
-  function handleLogout() {
-    signOut();
+  const [isOpen, setOpen] = useState(false);
+  const {
+    tickets,
+    isLoading,
+    error,
+    filters,
+    setFilters,
+    goToPage,
+    pagination,
+  } = useTicketStore();
+
+  function closeModal() {
+    setOpen(false);
   }
 
+  if (isLoading) return <p>Carregando tickets...</p>;
+  if (error) return <p>Erro: {error}</p>;
   return (
     <>
-      <Button onClick={handleLogout}>Sair</Button>
+      <Title icon={<FiMessageSquare size={25} />}>Atendimentos</Title>
+      <>
+        <div className="dashboard-new-area">
+          <button
+            onClick={() => {
+              setOpen(true);
+            }}
+            className="dashboard-new-btn"
+          >
+            <FiPlus size={25} /> Novo Atendimento
+          </button>
+        </div>
+        <Section>
+          <table>
+            <caption>Lista de Atendimentos</caption>
+            <thead>
+              <tr>
+                <th scope="col" colSpan={2}>
+                  Cliente
+                </th>
+                <th scope="col">CNPJ</th>
+                <th scope="col">Assunto</th>
+                <th scope="col">Status</th>
+                <th scope="col">Cadastro</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {tickets &&
+                tickets.map((item, index) => (
+                  <tr key={index}>
+                    <td data-label="Cliente" colSpan={2}>
+                      {item.customer.tradeName}
+                    </td>
+                    <td data-label="CNPJ">{item.customer.cnpj}</td>
+                    <td data-label="Assunto">{item.topic}</td>
+                    <td data-label="Status">
+                      <span
+                        className="badge"
+                        style={{ backgroundColor: "#999" }}
+                      >
+                        {item.status}
+                      </span>
+                    </td>
+                    <td data-label="Cadastro">
+                      {new Date(item.created_at).toLocaleDateString()}
+                    </td>
+                    <td data-label="">
+                      <button
+                        className="action"
+                        style={{ backgroundColor: "#3583f6" }}
+                      >
+                        <FiSearch size={17} />
+                      </button>
+                      <button
+                        className="action"
+                        style={{ backgroundColor: "#f6a935" }}
+                      >
+                        <FiEdit2 size={17} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          <div>
+            <Pagination
+              currentPage={pagination.current_page}
+              lastPage={pagination.last_page}
+              onPageChange={goToPage}
+              itemPerPage={filters.limit}
+              orderBy={filters.order_by}
+              order={filters.order || "desc"}
+              onLimitChange={(limit) => setFilters({ limit })}
+              onOrderingChange={(order_by) => setFilters({ order_by })}
+              onChangeOrder={(order) => {
+                setFilters({ order });
+              }}
+            />
+          </div>
+        </Section>
+        <Modal isOpen={isOpen} title="Novo chamado" onClose={closeModal}>
+          <NewCalling onClose={closeModal} />
+        </Modal>
+      </>
     </>
   );
 }
