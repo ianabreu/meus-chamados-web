630789d67e33f09f042b3733634519ebaf26d7b8
Ian Abreu <ianabreudev@gmail.com>
2025-01-30 22:48:23 -0300

    add ticketServices and CustomerServices get function

--- src/services/customerServices.ts
+++ src/services/customerServices.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-catch */
 import { createCustomerDTO, Customer } from "../@types/Customer";
+import { queryParamsProps } from "../@types/Pagination";
 import { api } from "./apiConfig";
 
 const customerServices = {
@@ -7,6 +8,16 @@ const customerServices = {
     try {
       const response = await api.post("/customers", data);
       return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+  async get(queryParams: queryParamsProps): Promise<Customer[]> {
+    try {
+      const response = await api.get(`/customers`, {
+        params: queryParams,
+      });
+      return response.data.results;
     } catch (error) {
       throw error;
     }
---
+++ src/services/ticketServices.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-useless-catch */
+import { PaginationResult, queryParamsProps } from "../@types/Pagination";
+import { createTicketDTO, Ticket } from "../@types/Ticket";
+import { api } from "./apiConfig";
+
+const ticketServices = {
+  async create(data: createTicketDTO): Promise<Ticket> {
+    try {
+      const response = await api.post("/tickets", data);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+  async get(queryParams: queryParamsProps): Promise<PaginationResult<Ticket>> {
+    try {
+      const response = await api.get(`/tickets`, {
+        params: queryParams,
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export { ticketServices };
